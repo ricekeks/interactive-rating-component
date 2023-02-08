1aaf5fe5b78ef26a92f13798c21fdf6104120814
ricekeks <107611633+ricekeks@users.noreply.github.com>
2023-02-08 13:33:37 +0100

    Initial commit
    Added all files for full functionality.

---
+++ js/rating.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+  (function () {
+    let rating = document.querySelectorAll('.my-rating');
+    let i = 0;
+    //loop through stars
+    while (i < rating.length) {
+      //attach click event
+      rating[i].addEventListener('click', function () {
+        //current value
+        let val = parseInt(this.getAttribute("rating-value"));
+        //display recently clicked value
+        document.querySelector('#ratingdisplay').value = 'You selected ' + val + ' out of 5';
+      });
+      i++;
+    } //end of while loop
+  })(); // end of rate function
+
+  (function () {
+    var submitbttn = document.getElementById("submitbttn");
+    submitbttn.onclick = function(){
+      if(document.querySelector('#ratingdisplay').value != ''){
+        console.log("Button pressed");
+        var ratingbox = document.querySelector(".userrating");
+        var thankyoubox = document.querySelector(".thankyou");
+
+        ratingbox.style.display = "none";
+        thankyoubox.style.display = "block";
+      }
+
+      return false;
+    }
+  })(); // end of submitrating function
+});
